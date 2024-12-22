b26d9cda37dee72d50c559164cfe8468d1ac9724
m4tris <huseyinburak.tetikk@gmail.com>
2024-12-23 01:12:27 +0300

    third commit : login scrren and edits

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -11,31 +12,31 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Giriş Yap</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          E-posta:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Şifre:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit">Giriş Yap</button>
-      </form>
+    <div className='login-container'>
+      <div className='login-form'>
+        <h1>Giriş Yap</h1>
+        <form onSubmit={handleLogin}>
+          <div className='input-group'>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label className={email ? 'filled' : ''}>E-posta</label>
+          </div>
+          <div className='input-group'>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label className={password ? 'filled' : ''}>Şifre</label>
+          </div>
+          <button type="submit">Giriş Yap</button>
+        </form>
+      </div>
     </div>
   );
 };
